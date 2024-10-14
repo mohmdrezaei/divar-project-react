d4703430ac86e8820b30289d50df905a722d587b
mohmdrezaei <mhwmdrz9@gmail.com>
2024-10-14 23:53:18 +0330

    First step form to get user number

--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -1,8 +1,26 @@
-
-function SendOtpForm() {
+function SendOtpForm({ mobile, setMobile, setStep }) {
+  const submitHandler = (event) => {
+    event.preventDefault()
+    console.log(event);
+    
+  };
   return (
-    <div>SendOtpForm</div>
-  )
+    <form onSubmit={submitHandler}>
+      <p>ورود به حساب کاربری</p>
+      <span>
+        برای استقاده از امکانات دیوار ، لطفا شمارهموبایل خود را وارد کنید. کد
+        تایید به این شماره ارسال خواهد شد
+      </span>
+      <label htmlFor="input">شماره موبایل خود راوارد کنید</label>
+      <input
+        type="text"
+        placeholder="شماره موبایل"
+        value={mobile}
+        onChange={(e) => setMobile(e.target.value)}
+      />
+      <button type="submit">ارسال کد تایید</button>
+    </form>
+  );
 }
 
-export default SendOtpForm
+export default SendOtpForm;

c0af27492db2be6f990d3c6ca5a6262c53a320a2
mohmdrezaei <mhwmdrz9@gmail.com>
2024-10-18 14:10:12 +0330

    The second step form to receive SMS OTP

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,8 +1,25 @@
+function CheckOtpForm({ code, setCode, mobile, setStep }) {
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    console.log({code , mobile})
 
-function CheckOtpForm() {
+  }
   return (
-    <div>CheckOtpForm</div>
-  )
+    <form onSubmit={submitHandler}>
+      <p>تایید کد پیامک شده</p>
+      <span>کد پیامک شده به شماره {mobile} را وارد کنید </span>
+      <label htmlFor="input">کد تایید را وارد کنید</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="کد تایید"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button type="subnit">ورود</button>
+      <button onClick={()=>setStep(1)}>تغییر شماره موبایل</button>
+    </form>
+  );
 }
 
-export default CheckOtpForm
+export default CheckOtpForm;
